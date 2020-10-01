c07a51b699e98538b2ca8dee1cfe9e04f7648d41
Ben Haus <ben@little-haus.com>
2020-10-01 17:57:08 -0400

    settign up app and installing dependencies

--- food-truck-tracker/src/App.js
+++ food-truck-tracker/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+        <header className="App-header">
+          <p>App Header</p>
+        </header>
+        </Col>
+      </Row>
+
+      <section>
+        <p>App Body</p>
+        <Button>Btn Test</Button>
+      </section>
+
+    </Container>
   );
 }
 
